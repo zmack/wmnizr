6841e9922a3744444ce451e703ba6c40921030f2
Andrei Bocan <zmaxor@gmail.com>
2009-11-23 16:53:08 +0200

    Use fancy datamapper fanciness to filter posts

--- models/post.rb
+++ models/post.rb
@@ -12,10 +12,27 @@ class Post
   property :title, String
   property :permalink, String
   property :published_at, DateTime
+
+  def self.published
+    self.all(:conditions => ["published_at is not null"])
+  end
+
+  def self.by_year(year)
+    self.all(:conditions => ["strftime('%Y', published_at) = '#{year.to_i}'"])
+  end
   
   def fill_permalink
     return if title.nil?
     self.permalink = 
       (Iconv.new('US-ASCII//TRANSLIT', 'utf-8').iconv title).gsub(/[^\w\s\-\â€”]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase  
+  end
+
+
+  def published?
+    !self.published_at.nil?
+  end
+
+  def published=(value)
+    self.published_at = Time.now if value
   end
 end
--- test/post_test.rb
+++ test/post_test.rb
@@ -21,4 +21,31 @@ class PostTest < WmnizrTest
 
     assert_equal 'fancyen-footwork', @post.permalink
   end
+
+  def test_fill_published_at_on_attributing_published
+    @post = Post.new :title => "foo bar", :published => true
+
+    assert_not_equal nil, @post.published_at 
+  end
+
+  def test_dont_fill_published_at_on_attributing_published_as_false
+    @post = Post.new :title => "foo bar", :published => false
+
+    assert_equal nil, @post.published_at 
+  end
+
+  def test_return_published_posts
+    @post = Post.create :title => 'foo', :published => true
+    @post = Post.create :title => 'foo', :published => false
+
+    assert_equal 1, Post.published.count
+  end
+
+  def test_return_published_posts_by_year
+    @post = Post.create :title => 'foo', :published_at => Date.parse('11-11-2005')
+    @post = Post.create :title => 'foo', :published => true
+
+    assert_equal 1, Post.by_year(2005).count
+    assert_equal 1, Post.by_year(2009).count
+  end
 end
