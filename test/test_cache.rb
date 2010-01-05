d269ee9747de38d91455086470d2e667715d79f9
Andrei Bocan <zmaxor@gmail.com>
2010-01-05 14:28:35 +0200

    Isolate tests =/

--- test/test_cache.rb
+++ test/test_cache.rb
@@ -6,25 +6,30 @@ class CacheTester
 
   def initialize
     @request = Struct.new(:path_info).new('foo')
+    @hostname = "pickle"
   end
 end
 
 class CacheTest < Test::Unit::TestCase
   def setup
     @cache = CacheTester.new
-  end
-
-  def test_creates_cache_folder_when_it_does_not_exist
     FakeFS.activate!
     @cache.cache('foobarbaz')
-    assert File.exist?('cache/foo')
-    assert_equal 'foobarbaz', File.read('cache/foo')
+  end
+
+  def teardown
     FakeFS.deactivate!
   end
 
+  def test_creates_cache_folder_when_it_does_not_exist
+    assert File.exist?('cache/pickle')
+  end
+
   def test_creates_cache_file
+    assert File.exist?('cache/pickle/foo')
   end
 
   def test_file_created_contains_the_same_text_as_original_content
+    assert_equal 'foobarbaz', File.read('cache/pickle/foo')
   end
 end
